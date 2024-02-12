39613656192251557a61284a6a0c3e2b932e977b
n0ted <bhadauriagaurav22@gmail.com>
2024-02-12 17:40:52 +0530

    dataset_update

--- ML_Training/Scripts/ML_Model/Priority_Model/Prio_pred_LR_ALGO.py
+++ ML_Training/Scripts/ML_Model/Priority_Model/Prio_pred_LR_ALGO.py
@@ -11,13 +11,19 @@ import re
 data = pd.read_csv('c:/Users/Lenovo/Downloads/Test/ML_Training/DataSet/sample.csv')
 
 
+# def process_dates(df):
+#     reference_date = pd.Timestamp('2023-01-01')
+#     for col in ['Created Date', 'Last Updated Date']:
+#         df[col] = pd.to_datetime(df[col])
+#         df[col] = (df[col] - reference_date).dt.days
+#     return df
+
 def process_dates(df):
     reference_date = pd.Timestamp('2023-01-01')
     for col in ['Created Date', 'Last Updated Date']:
         df[col] = pd.to_datetime(df[col])
         df[col] = (df[col] - reference_date).dt.days
     return df
-
 
 data = process_dates(data)
 
