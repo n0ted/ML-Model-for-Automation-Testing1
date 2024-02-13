9f2b7fe58aeded860a5fe06c1dd5e0d7669f2cb7
n0ted <bhadauriagaurav22@gmail.com>
2024-02-13 16:02:49 +0530

    TEST1

--- ML_Training/Scripts/ML_Model/Priority_Model/Prio_v2.py
+++ ML_Training/Scripts/ML_Model/Priority_Model/Prio_v2.py
@@ -63,7 +63,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 model_pipeline = load_model(X_train, y_train)
 
 # Predict priority for new test cases
-new_test_cases = pd.read_csv('ML_Training/DataSet/output.csv')
+new_test_cases = pd.read_csv('ML_Training/DataSet/input.csv')
 predicted_priorities = predict_priority_for_new_test_cases(new_test_cases)
 
 # Output predictions
